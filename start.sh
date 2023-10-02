97a32ea945ef2ee8824ca3d87e8960d4d1489fcb
Jeff Nelson <jeff.nelson@nelnet.net>
2023-10-02 15:45:50 -0500

    Updates

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## This file will create the RTMP/Encoding stack and web the web player in one shot
-
-## Setup RTMP/Encoding stack
-docker-compose up -d --force-recreate
-
-## Download the web player
-git clone https://github.com/netwarlan/webplayer.git ./stream/.
